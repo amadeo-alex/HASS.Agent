f208f55824c7f9f3c4f66f3615be71694aeb950e
Amadeo Alex <68441479+amadeo-alex@users.noreply.github.com>
2024-06-23 16:51:47 +0200

    store

---
+++ src/HASS.Agent/HASS.Agent.Base/Contracts/Managers/IHomeAssistantApiManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HASS.Agent.Base.Contracts.Managers;
+internal interface IHomeAssistantApiManager
+{
+    public const string EVENT_NOTIFICATION = "hass_agent_notifications";
+
+    public Task InitializeAsync();
+    public Task FireEventAsync();
+}
---
+++ src/HASS.Agent/HASS.Agent.Base/Managers/HomeAssistantApiManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HASS.Agent.Base.Contracts.Managers;
+
+namespace HASS.Agent.Base.Managers;
+internal class HomeAssistantApiManager : IHomeAssistantApiManager
+{
+    private readonly ISettingsManager _settingsManager;
+
+    public HomeAssistantApiManager(ISettingsManager settingsManager)
+    {
+        _settingsManager = settingsManager;
+
+    }
+
+    public Task InitializeAsync()
+    {
+
+    }
+    public Task FireEventAsync()
+    {
+    }
+}
